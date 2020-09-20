50eca9d868e8da9d92fb59e580389340d0f5bdc2
ageng123 <ageng133@gmail.com>
2020-09-21 00:23:23 +0700

    Update autoload.php, config.php, and 4 more files...

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -88,7 +88,7 @@ $autoload['drivers'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('template', 'date');
+$autoload['helper'] = array('template', 'date', 'url');
 
 
 /*
