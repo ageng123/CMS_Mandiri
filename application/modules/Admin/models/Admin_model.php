14e360ecb0716d6add7eee7c91845164a4ce39d4
ageng123 <ageng133@gmail.com>
2020-09-23 09:49:32 +0700

    Update Admin_model.php, __quick-template.php, and 41 more files...

--- application/modules/Admin/models/Admin_model.php
+++ application/modules/Admin/models/Admin_model.php
@@ -1,7 +1,6 @@
 <?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
 require_once(APPPATH.'/models/Main.php');
-
 class Admin_model extends Main{
     protected $table = 'tb_user';
     protected $primaryKey = 'id_user';
