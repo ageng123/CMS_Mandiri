9b7994d5a445ca01daf046d821bc05d7a286f0cc
Ali Akbar <akbarali04id@gmail.com>
2020-09-23 16:12:03 +0700

    ganti nama user database, masukin template admin

--- application/modules/Admin/models/Admin_model.php
+++ application/modules/Admin/models/Admin_model.php
@@ -1,12 +1,11 @@
 <?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
 require_once(APPPATH.'/models/Main.php');
-class Admin_model extends Main{
-    protected $table = 'tb_user';
-    protected $primaryKey = 'id_user';
-    public function __construct()
-    {
-        parent::__construct();
-    }
+    class Admin_model extends Main{
+        protected $table = 'tb_user';
+        protected $primaryKey = 'id_user';
+        public function __construct(){
+            parent::__construct();
+        }
 
-}
+    }
