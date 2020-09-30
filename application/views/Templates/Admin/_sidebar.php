3e38385eb5228195b609074bfa8a1d71996a351b
ageng123 <ageng133@gmail.com>
2020-09-30 09:07:37 +0700

    Update Admin Page

--- application/views/Templates/Admin/_sidebar.php
+++ application/views/Templates/Admin/_sidebar.php
@@ -1,36 +1,36 @@
 <!-- Sidenav -->
 <nav class="sidenav navbar navbar-vertical fixed-left navbar-expand-xs navbar-light bg-white" id="sidenav-main">
 
-    <div class="scrollbar-inner">
+  <div class="scrollbar-inner">
 
-      <!-- Brand -->
-      <div class="sidenav-header d-flex align-items-center">
-        <a class="navbar-brand" href="dashboard.html">
-           <img src="<?= base_url('assets/new_template/') ?>img/logo.png" class="navbar-brand-img"> 
-           <!-- <h1 style="font-weight: bold; color: #eba502;">PANEN SAHAM</h1>    -->
-        </a>
+    <!-- Brand -->
+    <div class="sidenav-header d-flex align-items-center">
+      <a class="navbar-brand" href="dashboard.html">
+        <img src="<?= base_url('assets/new_template/') ?>img/logo.png" class="navbar-brand-img">
+        <!-- <h1 style="font-weight: bold; color: #eba502;">PANEN SAHAM</h1>    -->
+      </a>
 
-        <div class="ml-auto">
-          <!-- Sidenav toggler -->
-          <div class="sidenav-toggler d-none d-xl-block" data-action="sidenav-unpin" data-target="#sidenav-main">
-            <div class="sidenav-toggler-inner">
-              <i class="sidenav-toggler-line"></i>
-              <i class="sidenav-toggler-line"></i>
-              <i class="sidenav-toggler-line"></i>
-            </div>
+      <div class="ml-auto">
+        <!-- Sidenav toggler -->
+        <div class="sidenav-toggler d-none d-xl-block" data-action="sidenav-unpin" data-target="#sidenav-main">
+          <div class="sidenav-toggler-inner">
+            <i class="sidenav-toggler-line"></i>
+            <i class="sidenav-toggler-line"></i>
+            <i class="sidenav-toggler-line"></i>
           </div>
         </div>
       </div>
+    </div>
 
-      <div class="navbar-inner">
-        <!-- Collapse -->
-        <div class="collapse navbar-collapse" id="sidenav-collapse-main">
+    <div class="navbar-inner">
+      <!-- Collapse -->
+      <div class="collapse navbar-collapse" id="sidenav-collapse-main">
 
-          <!-- Nav items -->
-          <ul class="navbar-nav">
+        <!-- Nav items -->
+        <ul class="navbar-nav">
 
-            <!-- MENU COLLAPSE/DI KLIK MENUNYA TURUN KE BAWAH -->
-            <!-- <li class="nav-item">
+          <!-- MENU COLLAPSE/DI KLIK MENUNYA TURUN KE BAWAH -->
+          <!-- <li class="nav-item">
               <a class="nav-link active" href="#navbar-dashboards" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="navbar-dashboards">
                 <i class="ni ni-shop text-primary"></i>
                 <span class="nav-link-text">Dashboards</span>
@@ -48,39 +48,58 @@
               </div>
             </li> -->
 
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <i class="ni ni-shop text-primary"></i>
-                <span class="nav-link-text">Dashboard</span>
-              </a>
-            </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="ni ni-shop text-primary"></i>
+              <span class="nav-link-text">Dashboard</span>
+            </a>
+          </li>
 
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <i class="ni ni-archive-2 text-green"></i>
-                <span class="nav-link-text">Menu A</span>
-              </a>
-            </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="ni ni-archive-2 text-green"></i>
+              <span class="nav-link-text">Menu A</span>
+            </a>
 
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <i class="ni ni-spaceship text-pink"></i>
-                <span class="nav-link-text">Menu B</span>
-              </a>
-            </li>
+          </li>
 
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <i class="ni ni-planet text-info"></i>
-                <span class="nav-link-text">Menu C</span>
-              </a>
-            </li>
-            
-          </ul>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="ni ni-spaceship text-pink"></i>
+              <span class="nav-link-text">Menu B</span>
+            </a>
+          </li>
 
-        </div> 
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="ni ni-planet text-info"></i>
+              <span class="nav-link-text">Menu C</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <h6 class="navbar-heading p-2 text-mmuted">
+              <span class="docs-normal">Content</span>
+            </h6>
+          </li>
+          <li class="nav-item"><a href="" class="nav-link">Berita</a></li>
+          <li class="nav-item"><a href="" class="nav-link">Product</a></li>
+          <hr class="my-3">
+          <li class="nav-item">
+            <h6 class="navbar-heading p-2 text-mmuted">
+              <span class="docs-normal">Administration</span>
+            </h6>
+          </li>
+          <li class="nav-item"><a href="" class="nav-link">User</a></li>
+          <li class="nav-item"><a href="" class="nav-link">Role</a></li>
+          <li class="nav-item"><a href="" class="nav-link">Assign Role</a></li>
+          <li class="nav-item"><a href="" class="nav-link">Menu List</a></li>
+          <li class="nav-item"><a href="" class="nav-link">Access List</a></li>
+          <li class="nav-item"><a href="" class="nav-link">Contact List</a></li>
+        </ul>
+
       </div>
-
     </div>
+
+  </div>
 
 </nav>
