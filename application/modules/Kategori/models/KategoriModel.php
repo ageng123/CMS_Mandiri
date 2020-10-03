6eb983a00ccca790b996b41dba63663426b57e62
ageng123 <ageng133@gmail.com>
2020-10-03 11:49:58 +0700

    Module Kategori Berita dan Kategori Produk - export excel belum dan belum ada delete banyak.

--- application/modules/Kategori/models/KategoriModel.php
+++ application/modules/Kategori/models/KategoriModel.php
@@ -1,12 +1,13 @@
 <?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
  require_once(APPPATH.'/models/Main.php');
-    class Landing_Model extends Main{
-        protected $table = 'tb_user';
-        protected $primaryKey = 'id_user';
+    class KategoriModel extends Main{
+        protected $table = 'kategori_list';
+        protected $primaryKey = 'id_kategori';
         
         public function __construct(){
             parent::__construct();
+            $this->last_update = date('Y-m-d H:i:s');
         }
 
     }
