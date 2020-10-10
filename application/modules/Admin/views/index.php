af784eddf5c66aad20fa302e2181f8b384349940
Ali Akbar <akbarali04id@gmail.com>
2020-10-11 02:56:37 +0700

    buat tampilan dashboard admin sementara

--- application/modules/Admin/views/index.php
+++ application/modules/Admin/views/index.php
@@ -2,47 +2,108 @@
     <div class="container-fluid">
       <div class="header-body">
         <div class="row align-items-center py-3">
-
+          <div class="col-lg-6 col-7">
+            <h6 class="h2 text-white d-inline-block mb-0">Dashboard</h6>
+            </nav>
+          </div>
         </div>
       </div>
     </div>
 </div>
 
 <div class="container-fluid mt--6">
-<div class="row">
-  <div class="col">
-    <div class="card">
-      <div class="card-header">
-        <div class="row">
-            <div class="col-6">
-                <h3>Nama Judul</h3>
+  <div class="row">
+    <div class="col-xl-3 col-md-6">
+      <div class="card card-stats">
+        <!-- Card body -->
+        <div class="card-body">
+          <div class="row">
+            <div class="col">
+              <h5 class="card-title text-uppercase text-muted mb-0">Total traffic</h5>
+              <span class="h2 font-weight-bold mb-0">350,897</span>
             </div>
-            <div class="col-6 text-right"> 
-              <a class="btn btn-success btn-sm" data-toggle="tooltip" data-placement="top" title="Add" href="#"> <span class="btn-inner--text"><i class="fa fa-plus"></i></span> </a>
-              <a class="btn btn-success btn-sm" data-toggle="tooltip" data-placement="top" title="Export Excel" href="#"> <span class="btn-inner--text"><i class="fa fa-file-excel"></i></span> </a>
+            <div class="col-auto">
+              <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">
+                <i class="ni ni-active-40"></i>
+              </div>
             </div>
+          </div>
+          <p class="mt-3 mb-0 text-sm">
+            <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+            <span class="text-nowrap">Since last month</span>
+          </p>
         </div>
       </div>
- 
-      <div class="table-responsive py-4">
-        <table class="table table-flush init-DataTable" data-url="<?= base_url('Admin/getJSON') ?>" id="userTbl">
-          <thead class="thead-light">
-            <tr>
-              <th></th>
-              <th class="text-center" style="padding: 1.3vh"><input type="checkbox" class="form-check-input all-checkbox-dt" style="position: inherit; margin: auto" name="" id=""></th>
-              <th>No</th>
-              <th>Kolom A</th>
-              <th>Kolom B</th>
-              <th>Kolom C</th>
-              <th>Aksi</th>
-            </tr>
-          </thead>
-          <tbody>
-          </tbody>
-        </table>
+    </div>
+    <div class="col-xl-3 col-md-6">
+      <div class="card card-stats">
+        <!-- Card body -->
+        <div class="card-body">
+          <div class="row">
+            <div class="col">
+              <h5 class="card-title text-uppercase text-muted mb-0">New users</h5>
+              <span class="h2 font-weight-bold mb-0">2,356</span>
+            </div>
+            <div class="col-auto">
+              <div class="icon icon-shape bg-gradient-orange text-white rounded-circle shadow">
+                <i class="ni ni-chart-pie-35"></i>
+              </div>
+            </div>
+          </div>
+          <p class="mt-3 mb-0 text-sm">
+            <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+            <span class="text-nowrap">Since last month</span>
+          </p>
+        </div>
       </div>
     </div>
+    <div class="col-xl-3 col-md-6">
+      <div class="card card-stats">
+        <!-- Card body -->
+        <div class="card-body">
+          <div class="row">
+            <div class="col">
+              <h5 class="card-title text-uppercase text-muted mb-0">Sales</h5>
+              <span class="h2 font-weight-bold mb-0">924</span>
+            </div>
+            <div class="col-auto">
+              <div class="icon icon-shape bg-gradient-green text-white rounded-circle shadow">
+                <i class="ni ni-money-coins"></i>
+              </div>
+            </div>
+          </div>
+          <p class="mt-3 mb-0 text-sm">
+            <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+            <span class="text-nowrap">Since last month</span>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-xl-3 col-md-6">
+      <div class="card card-stats">
+        <!-- Card body -->
+        <div class="card-body">
+          <div class="row">
+            <div class="col">
+              <h5 class="card-title text-uppercase text-muted mb-0">Performance</h5>
+              <span class="h2 font-weight-bold mb-0">49,65%</span>
+            </div>
+            <div class="col-auto">
+              <div class="icon icon-shape bg-gradient-info text-white rounded-circle shadow">
+                <i class="ni ni-chart-bar-32"></i>
+              </div>
+            </div>
+          </div>
+          <p class="mt-3 mb-0 text-sm">
+            <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+            <span class="text-nowrap">Since last month</span>
+          </p>
+        </div>
+      </div>
+    </div>
+    
   </div>
-</div>
+
+
 
 
