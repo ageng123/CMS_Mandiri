2ff12ee9699f9be06af281f558ec683bb14bcd65
ageng123 <ageng133@gmail.com>
2020-10-12 23:53:19 +0700

    Update form_pendaftaran.php and landing.js

--- application/modules/Landing/views/form_pendaftaran.php
+++ application/modules/Landing/views/form_pendaftaran.php
@@ -11,18 +11,35 @@
             <label for="">Apakah Sudah Menjadi Member Komunitas PanenSaham ?</label><br>
             <div class="w-50 daftar-radio">
               <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
+                  value="option1">
                 <label class="form-check-label" for="inlineRadio1">Sudah</label>
               </div>
               <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2"
+                  value="option2">
                 <label class="form-check-label" for="inlineRadio2">Belum</label>
               </div>
             </div>
           </div>
           <div class="form-group w-50 d-flex flex-row">
-              <label style="min-width: 4vw" for="">Client Id</label>
-              <input type="text" name="client_id" id="" class="form-control">
+            <label style="min-width: 4vw" for="">Client Id</label>
+            <input type="text" name="client_id" id="" class="form-control">
+          </div>
+          <div class="form-group">
+            <label for="">Apakah Sudah Memiliki Kartu Tanda Penduduk (diatas 17 tahun)?</label> <br>
+            <div class="w-50 daftar-radio">
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions2" id="inlineRadio1"
+                  value="option1">
+                <label class="form-check-label" for="inlineRadio1">Sudah</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions2" id="inlineRadio2"
+                  value="option2">
+                <label class="form-check-label" for="inlineRadio2">Belum</label>
+              </div>
+            </div>
           </div>
           <div class="form-group">
             <table>
@@ -38,19 +55,6 @@
                   <input type="file" name="" id=""></td>
               </tr>
             </table>
-          </div>
-          <div class="form-group">
-            <label for="">Apakah Sudah Memiliki Kartu Tanda Penduduk (diatas 17 tahun)?</label> <br>
-            <div class="w-50 daftar-radio">
-              <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="inlineRadioOptions2" id="inlineRadio1" value="option1">
-                <label class="form-check-label" for="inlineRadio1">Sudah</label>
-              </div>
-              <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="inlineRadioOptions2" id="inlineRadio2" value="option2">
-                <label class="form-check-label" for="inlineRadio2">Belum</label>
-              </div>
-            </div>
           </div>
           <div class="form-row">
             <div class="col">
@@ -77,13 +81,15 @@
             <p>(Seluru Data Formulir <b class="red">Wajib</b> diisi dengan lengkap dan benar) </p>
           </div>
           <div class="form-group row">
-            <label for="colFormLabelSm" class="col-sm-5 col-form-label col-form-label">Nama Lengkap Sesuai Identitas</label>
+            <label for="colFormLabelSm" class="col-sm-5 col-form-label col-form-label">Nama Lengkap Sesuai
+              Identitas</label>
             <div class="col-sm-7">
               <input type="text" class="form-control" id="colFormLabelSm" name="nama" placeholder="">
             </div>
           </div>
           <div class="form-group row">
-            <label for="colFormLabelSm" class="col-sm-5 col-form-label col-form-label">Nomor Identitas (E-KTP / KTP AHLI WARIS / KK)</label>
+            <label for="colFormLabelSm" class="col-sm-5 col-form-label col-form-label">Nomor Identitas (E-KTP / KTP AHLI
+              WARIS / KK)</label>
             <div class="col-sm-7">
               <input type="text" class="form-control" id="colFormLabelSm" name="nomor_identity" placeholder="">
             </div>
@@ -110,13 +116,13 @@
             </div>
             <div class="col-md-5">
               <div class="row">
-                  <label for="" class="col-form-label col-md-5">Jenis Kelamin</label>
-                  <div class="col-md-6">
-                    <select name="jenis_kelamin" id="" class="form-control">
-                      <option value="l">Laki-Laki</option>
-                      <option value="p">Perempuan</option>
-                    </select>
-                  </div>
+                <label for="" class="col-form-label col-md-5">Jenis Kelamin</label>
+                <div class="col-md-6">
+                  <select name="jenis_kelamin" id="" class="form-control">
+                    <option value="l">Laki-Laki</option>
+                    <option value="p">Perempuan</option>
+                  </select>
+                </div>
               </div>
             </div>
           </div>
@@ -158,7 +164,7 @@
           </div>
           <div class="form-group row">
             <div class="col-md-6 row">
-              <label  class="col-form-label col-md-5" for="">Nomor Handphone</label>
+              <label class="col-form-label col-md-5" for="">Nomor Handphone</label>
               <div class="col-md-6">
                 <input type="text" name="hp">
               </div>
@@ -177,28 +183,156 @@
             </div>
           </div>
           <div class="form-group">
-            <label style="display: flex; flex-direction: row; width: 80%; justify-content: space-between" for="">Alamat Rumah
+            <label style="display: flex; flex-direction: row; width: 80%; justify-content: space-between" for="">Alamat
+              Rumah
               <div class="daftar-radio">
                 <div class="form-check form-check-inline">
                   <input class="form-check-input" type="radio" name="alamatradio" id="inlineRadio2" value="option2">
-                  <label class="form-check-label" for="inlineRadio2">Alamat Sama Dengan Alamat E-KTP / KTP Ahli Waris / KK</label>
+                  <label class="form-check-label" for="inlineRadio2">Alamat Sama Dengan Alamat E-KTP / KTP Ahli Waris /
+                    KK</label>
                 </div>
               </div>
             </label>
             <textarea name="alamat" id="" cols="30" rows="10" class="form-control"></textarea>
           </div>
         </section>
-        <h3>Profile</h3>
+        <h3>STEP 2</h3>
         <section>
-          <label for="name">First name *</label>
-          <input id="name" name="name" type="text" class="required form-control">
-          <label for="surname">Last name *</label>
-          <input id="surname" name="surname" type="text" class="required form-control">
-          <label for="email">Email *</label>
-          <input id="email" name="email" type="text" class="required email">
-          <label for="address">Address</label>
-          <input id="address" name="address" type="text">
-          <p>(*) Mandatory</p>
+          <h2>Data Pekerjaan</h2>
+          <div class="form-group row">
+            <div class="col-md-6">
+              <label for="">Pekerjaan</label>
+              <select class="form-control" name="pekerjaan[jenis]" id="">
+                <option value="">Programmer</option>
+                <option value="">IRT</option>
+              </select>
+            </div>
+            <div class="col-md-6">
+              <label for="">Nama Perusahaan</label>
+              <input type="text" class="form-control" name="pekerjaan[perusahaan]" id="">
+            </div>
+          </div>
+          <div class="form-group row">
+            <div class="col-md-6">
+              <label for="">Divisi</label>
+              <input type="text" class="form-control" name="pekerjaan[divisi]" id="">
+            </div>
+            <div class="col-md-6">
+              <label for="">Lama Bekerja</label>
+              <input type="text" class="form-control" name="pekerjaan[lama]" placeholder="ex. : 2 tahun / 6 bulan"
+                id="">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="">Alamat Perusahaan</label>
+            <textarea name="pekerjaan[alamat]" class="form-control" id="" cols="30" rows="10"
+              class="form-control"></textarea>
+          </div>
+          <div class="form-group row">
+            <div class="col-md-4">
+              <div class="form-group">
+                <label for="">Provinsi</label>
+                <select name="pekerjaan[provinsi]" class="form-control" id=""></select>
+              </div>
+              <div class="form-group">
+                <label for="">Kecamatan</label>
+                <input type="text" name="pekerjaan[kecamatan]" id="" class="form-control">
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-group">
+                <label for="">Kabupaten</label>
+                <select name="pekerjaan[kabupaten]" class="form-control" id=""></select>
+              </div>
+              <div class="form-group">
+                <label for="">Kelurahan</label>
+                <input type="text" name="pekerjaan[kelurahan]" id="" class="form-control">
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-group">
+                <label for="">RT / RW</label>
+                <input type="text" name="pekerjaan[rt]" id="" class="form-control">
+              </div>
+              <div class="form-group">
+                <label for="">Kode Pos</label>
+                <input type="text" name="pekerjaan[kodepos]" id="" class="form-control">
+              </div>
+            </div>
+          </div>
+          <h2>Data Rekening Pembayaran <br> Sisa Hasil Usaha (SHU)</h2>
+          <div class="form-group row">
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="">Nama Rekening</label>
+                <input type="text" name="shu[nama]" class="form-control">
+              </div>
+              <div class="form-group">
+                <label for="">Nama Bank</label>
+                <input type="text" name="shu[bank]" class="form-control">
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="">No Rekening</label>
+                <input type="text" name="shu[norek]" class="form-control">
+              </div>
+              <div class="form-group">
+                <label for="">Cabang</label>
+                <input type="text" name="shu[cabang]" class="form-control">
+              </div>
+            </div>
+          </div>
+          <h2>Data Ahli Waris</h2>
+          <div class="form-group">
+            <label for="">Nama Ahli Waris</label>
+            <input type="text" name="waris[nama]" id="" class="form-control">
+          </div>
+          <div class="form-group">
+            <label for="">Hubungan Ahli Waris</label>
+            <input type="text" name="waris[hubungan]" id="" class="form-control">
+          </div>
+          <h2>Data Simpanan</h2>
+          <table style="width: 100%">
+            <tr>
+              <td style="width: 70%" colspan="2"><b>1. Simpanan Pokok</b></td>
+              <td>RP. 100.000</td>
+            </tr>
+            <tr>
+              <td><b>2. Simpanan Wajib</b></td>
+              <td style="float: right">
+                <select name="wajib" id="select_wajib" class="form-control-sm">
+                  <?php for($i = 1; $i <= 12; $i++): ?>
+                  <option value="<?= $i ?>"> <?= $i.' Bulan' ?> </option>
+                  <?php endfor; ?>
+                </select>
+              </td>
+              <td id="wajib" data-biaya="15000">Rp. 15.000</td>
+            </tr>
+            <tr>
+              <td colspan="2"><b>3. Simpanan Sukarela</b></td>
+              <td class="row p-3">RP. <input id="sukarela" type="text" class="form-control-sm" name="sukarela"></td>
+            </tr>
+            <tr>
+              <td colspan="2"><b>Total Pembayaran</b></td>
+              <td id="biaya-total">RP. 115.000</td>
+            </tr>
+          </table>
+          <div class="daftar-radio">
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="radio" name="aggreement[ketentuan]" id="inlineRadio2"
+                value="option2">
+              <label class="form-check-label" for="inlineRadio2">Alamat Sama Dengan Alamat E-KTP / KTP Ahli Waris /
+                KK</label>
+            </div>
+          </div>
+          <div class="daftar-radio">
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="radio" name="aggreement[anggota]" id="inlineRadio2" value="option2">
+              <label class="form-check-label" for="inlineRadio2">Alamat Sama Dengan Alamat E-KTP / KTP Ahli Waris /
+                KK</label>
+            </div>
+          </div>
         </section>
         <h3>Hints</h3>
         <section>
--- public/assets/js/landing.js
+++ public/assets/js/landing.js
@@ -1,8 +1,47 @@
 $(document).ready(function(){
+    let wajib = 15000;
+    let sukarela = 0;
     var form = $("#step-form");
         form.children("div").steps({
             headerTag: "h3",
             bodyTag: "section",
             transitionEffect: "slideLeft",
         });
+    $('#select_wajib').on('change', function(){
+        let biaya = $('#wajib').attr('data-biaya');
+        let bulan = $(this).val();
+        wajib = biaya*bulan
+        Pembayaran_Function.renderToForm(wajib, sukarela);
+    })
+    $('#sukarela').on('keyup', function(){
+        let biaya = $(this).val();
+        biaya = biaya.replace(/[^,\d]/g, '', '');
+        biaya = parseInt(biaya);
+        sukarela = parseInt(biaya);
+        $(this).val(Pembayaran_Function.formatRupiah(biaya), '');
+        Pembayaran_Function.renderToForm(wajib, sukarela);
+
+    })
 })
+const Pembayaran_Function = {
+    renderToForm: function(wajib, sukarela){
+        let prefix = "Rp. ";
+        let value = wajib + sukarela + 100000;
+        $('#biaya-total').html(Pembayaran_Function.formatRupiah(value, prefix));
+    },
+    formatRupiah: function(angka, prefix){
+        var 	number_string = angka.toString(),
+		split	= number_string.split(','),
+		sisa 	= split[0].length % 3,
+		rupiah 	= split[0].substr(0, sisa),
+		ribuan 	= split[0].substr(sisa).match(/\d{1,3}/gi);
+		
+	if (ribuan) {
+		separator = sisa ? '.' : '';
+		rupiah += separator + ribuan.join('.');
+	}
+	
+	rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+	return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
+    }
+}
