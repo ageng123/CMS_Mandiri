43ea1ef6383aa933638f117e61114bebd5fd37a6
Ali Akbar <akbarali04id@gmail.com>
2020-10-28 01:56:32 +0700

    ganti icon user di page profile

--- application/modules/Profile/views/index.php
+++ application/modules/Profile/views/index.php
@@ -11,17 +11,21 @@
       <div class="col-12" style="margin-top: 3vh;">
 
      <div class="col-lg-3">
-          <div class="blog-item thum-hover border-radius-15 hidden background-white hvr-float hvr-sh2">
-            <div class="position-relative">
-             
-                <div class="item-thumbnail background-dark"><img src="<?= base_url('assets/img/');?>opening.jpg" alt=""></div>
-            
+          <div class="blog-item">
+            <div class="position-relative" style="background-color: #fafafa;">
+                <div class="item-thumbnail rounded-circle">
+                  <img src="<?= base_url('assets/img/');?>user.png">
+                  
+                </div>
+                <br>
+                <center>Welcome Back <b style="color: #f7a91e;"><?= $user->full_name ?></b></center>
             </div>
-
-               <div class="border-radius-15 width-50px padding-10px background-main-color text-white text-center position-absolute top-20px right-20px">
-                <a href="<?= base_url();?>profile/edit_foto" title="Ganti Foto" data-original-title="Ganti Foto"><i class="fa fa-setting"></i></a>
-              </div>
           </div>
+          
+          <div class="border-radius-15 width-50px padding-10px background-main-color text-white text-center position-absolute top-20px right-20px">
+            <a href="<?= base_url();?>profile/edit_foto" title="Ganti Foto" data-original-title="Ganti Foto"><i class="fa fa-cog"></i></a>
+          </div>
+
         </div>
         <br>
 
