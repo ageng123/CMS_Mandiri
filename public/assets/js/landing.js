7a295f9bb15750dd73bc9ee988303a4a9b21d6f6
Ageng <ageng133@gmail.com>
2020-11-01 23:25:47 +0700

    Update landing_custom.css and landing.js

--- public/assets/js/landing.js
+++ public/assets/js/landing.js
@@ -284,6 +284,7 @@ $(document).ready(function () {
         let content = '';
         content_data.map(function (val, index) {
             $('.getBeritaLatest[data-berita=' + (index + 1) + ']').attr('src', Content_Services.renderSource(val));
+            console.log($('.subtitle[data-index='+index+']'));
             $('.newestNewsImage[data-index='+index+']').attr('src', Content_Services.renderSource(val));
             $('.newestNews_container[data-index='+index+']').removeClass('hide');
             $('.title[data-index='+index+']').html('<b>'+val.title+'</b>');
