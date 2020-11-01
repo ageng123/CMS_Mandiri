533d7faab0ee904c7fff0dfc115b59a486d38aab
Ageng <ageng133@gmail.com>
2020-11-01 22:44:54 +0700

    Update Auth_Guard.php

--- application/controllers/Auth_Guard.php
+++ application/controllers/Auth_Guard.php
@@ -4,7 +4,7 @@ Class Auth_Guard extends Auth{
         function __construct()
         {
             parent::__construct();
-            if($this->ion_auth->logged_in() and $this->session->userdata('id_role') == NULL ){
+            if($this->ion_auth->logged_in() and (int)$this->session->userdata('id_role') != 3 ){
             } else {
                 return redirect(base_url('admin/login'));
 
