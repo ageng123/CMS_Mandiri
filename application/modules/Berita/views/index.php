256f3d3cd1dc79fb63cadc2aaa1f641b3338adc2
Ali Akbar <akbarali04id@gmail.com>
2020-11-02 03:39:36 +0700

    Update index.php

--- application/modules/Berita/views/index.php
+++ application/modules/Berita/views/index.php
@@ -13,6 +13,9 @@
             Forum diskusi dan berbagi berita dari dalam maupun luar negeri. Mulai dari ideologi, politik, ekonomi, sosial dan budaya. <b style="color: #f7a91e;">Subforum</b>: berita-dunia-hiburan.
           </div>
         </div>
+
+        
+
       <div class="col-12" style="margin-top: 3vh">
         <div class="row">
           <?php foreach($data as $key => $val): ?>      
@@ -33,8 +36,37 @@
         </div>
       </div>
 
+      <!-- <div class="col-12" style="margin-top: 3vh">
+        <div class="row">
+          <?php $id = 3; for($i = 1; $i <= 8 ; $i++): ?>
+            <div class="col-xs-12 newestNews_container hide col-lg-3" style="width: 100%; min-height: 400px;" data-index="<?= $id ?>">
+              <div class="blog-item thum-hover border-radius-15 hidden background-white hvr-float hvr-sh2">
+                <div class="position-relative">
+                  <a class="btn-detail" data-index="<?= $id ?>" href="<?= base_url('landing/detail_berita/')?>">
+                    <div class="item-thumbnail background-dark"><img style="max-height: 140px;" data-index="<?= $id ?>" class="newestNewsImage" src="<?= site_url('resources/Berita/') ?>" alt=""></div>
+                  </a>
+                </div>
+                <b><a data-index="<?= $id ?>" class="btn-detail title text-extra-large margin-tb-20px d-block padding-lr-30px"></a></b>
+                <center><p data-index="<?= $id ?>" class="subtitle">Subtitle</p></center>
+                <hr>
+                <center>
+                  <a class="btn btn-warning border-radius-30 btn-detail" data-index="<?= $id ?>" href="<?= base_url('landing/detail_berita/')?>">
+                    Read More
+                  </a>
+                </center>
+                <hr class="margin-bottom-0px border-white">
 
-        <br>
+              </div>
+            </div>
+          <?php $id++ ?>
+          <?php endfor; ?>
+        </div>
+      </div> -->
+
+
+
+
+      <br>
       <p>
     <div class="row">
         <div class="col">
