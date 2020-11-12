974bcf39a5f8b87a7f1cb85888492c4572076017
Ali Akbar <akbarali04id@gmail.com>
2020-11-13 01:18:13 +0700

    Update index.php

--- application/modules/About/views/index.php
+++ application/modules/About/views/index.php
@@ -1,141 +1,191 @@
-    
-  <!-- // Page title -->
+    <!-- // Page title -->
 
-  <!-- page output -->
-
-  <div class="padding-tb-40px background-light-grey">
-    <div class="container">
-            <h1 class="widget-title clearfix"><b>TENTANG <font color="#f7a91e">KJKPI</font></b></h1>
-            <hr style="background-color: #f7a91e;width: 12%;height: 4px;" border="0" align="left">
-      <div class="row">
+    <!-- page output -->
+    <div class="padding-tb-40px background-light-grey">
+      <div class="container">
+        <h1 class="widget-title clearfix"><b>TENTANG <font color="#f7a91e">KJKPI</font></b></h1>
+        <hr style="background-color: #f7a91e;width: 12%;height: 4px;" border="0" align="left">
+        <div class="row">
 
 
-        <!-- sidebar -->
-        <div class="col-lg-4 col-md-4 sticky-sidebar">
+          <!-- sidebar -->
+          <div class="col-lg-4 col-md-4 sticky-sidebar">
 
-          <!-- Mail list -->
-          <div class="widget">
-           <p align="center"> <img src="<?= base_url('assets/img/about/') ?>owner2.jpeg" style="text-align: center;"></p>
-            <h4 class="widget-title clearfix"><span><font color="#f7a91e">FATH ALIANSYAH BUDIMAN</font></span></h4>
-            <p></p>
-            <p align="center">Founder and Managing Partner Of Koperasi Jasa Komunitas Panen Saham Indonesia</p><hr>
+            <!-- Mail list -->
+            <div class="widget">
+              <p align="center"> <img src="<?= base_url('assets/img/about/') ?>owner2.jpeg" style="text-align: center;">
+              </p>
+              <h4 class="widget-title clearfix"><span>
+                  <font color="#f7a91e">FATH ALIANSYAH BUDIMAN</font>
+                </span></h4>
+              <p></p>
+              <p align="center">Founder and Managing Partner Of Koperasi Jasa Komunitas Panen Saham Indonesia</p>
+              <hr>
 
-            <h4 class="widget-title clearfix"><span><font color="#f7a91e">Koperasi Jasa Komunitas Panen Saham Indonesia</font></span></h4>
-            <p align="center">merupakan salah satu upaya sebagai koperasi konsumen, namun juga mampu meningkatkan kinerja melalui anak perusahaan yang bergerak dalam berbagai bidang.</p>
-
-          </div>
-          <!-- //  Mail list -->
-
-
-
-
-        </div>
-        <!-- // sidebar -->
-
-        <!--  content -->
-        <div class="col-lg-8 col-md-8 sticky-content">
-
-          <!-- post -->
-          <div class="blog-entry background-white border-1 border-grey-1 margin-bottom-35px">
-            <div class="row">
-              <div class="col-lg-3"><img  src="<?= base_url('assets/img/about/') ?>visi.png" style="width: 80%;padding-left: 30px;"></div>
-              <div class="col-lg-9">
-                <div class="padding-25px">
-                <h3>Menjadi koperasi jasa yang <b>berkualitas</b> dan <b>terbaik</b> di Indonesia.</h3></div>
-              </div>
-              </div>
-<div class="row"> 
-            <div class="col-lg-3"><img  src="<?= base_url('assets/img/about/') ?>misi.png" style="width: 80%;padding-left: 30px;"></div>
-              <div class="col-lg-9"  style="border: 0px white solid;">
-                
-                <table style="border: 0px white solid;width: 100%;">
-                  <tr>
-                    <td style="border: 0px white solid;width: 10%;vertical-align: top;"><img  src="<?= base_url('assets/img/about/') ?>ceklis.png"></td>
-                    <td style="border: 0px white solid;width: 90%;vertical-align: top;text-align: justify;"><b style="font-size: 19px;">Mendidik</b> para anggotanya agar dapat menemukan potensinya dan memberdayakan diri melalui koperasi.</td>
-                  </tr>
-                  <tr>
-                    <td style="border: 0px white solid;vertical-align: top;"><img  src="<?= base_url('assets/img/about/') ?>ceklis.png"></td>
-                    <td style="border: 0px white solid;vertical-align: top;text-align: justify;"><b style="font-size: 19px;">Menciptakan</b> kesejahteraan bagi para anggota yang berkesinambungan. </td>
-                  </tr>
-                  <tr>
-                    <td style="border: 0px white solid;vertical-align: top;"><img  src="<?= base_url('assets/img/about/') ?>ceklis.png"></td>
-                    <td style="border: 0px white solid;vertical-align: top;text-align: justify;"><b style="font-size: 19px;">Memberikan</b> kontribusi bagi perkembangan koperasi di Indonesia. </td>
-                  </tr>
-                  <tr>
-                    <td style="border: 0px white solid;vertical-align: top;"><img  src="<?= base_url('assets/img/about/') ?>ceklis.png"></td>
-                    <td style="border: 0px white solid;vertical-align: top;text-align: justify;"><b style="font-size: 19px;">Mengelola</b> unit-unit usahanya secara professional dan akuntabel dengan menerapkan prinsip-prinsip pengelolaan perusahaan yang baik. </td>
-                  </tr>
-                  <tr>
-                    <td style="border: 0px white solid;vertical-align: top;"><img  src="<?= base_url('assets/img/about/') ?>ceklis.png"></td>
-                    <td style="border: 0px white solid;vertical-align: top;text-align: justify;"><b style="font-size: 19px;">Menjadi Mitra</b> terpercaya dan strategis bagi setiap anggota KJKPI </td>
-                  </tr>
-                </table>
-                </div>
-                </div>
-                 
-                
-                <div class="row">
-                
-                <div class="col-lg-3"><img  src="<?= base_url('assets/img/about/') ?>tujuan.png" style="padding-left: 30px;"></div>
-                </div> 
-              <div class="col-lg-12"  style="border: 0px white solid;padding-left: 30px;">
-                
-                <table style="border: 0px white solid;width: 100%;">
-                  <tr>
-                    <td style="border: 0px white solid;width: 8%;vertical-align: top;"><img  src="<?= base_url('assets/img/about/') ?>ceklis.png"></td>
-                    <td style="border: 0px white solid;width: 90%;vertical-align: top;text-align: justify;"><b style="font-size: 19px;">Koperasi bertujuan</b> meningkatkan kesejahteraan anggota pada khususnya dan masyarakat pada umumnya, sekaligus pada bagian yang tidak terpisahkan dari <b style="font-size: 19px;">tatanan perekonomian nasional demokratis dan berkeadilan.</b></td>
-                  </tr>
-                  <tr>
-                    <td style="border: 0px white solid;vertical-align: top;"><img  src="<?= base_url('assets/img/about/') ?>ceklis.png"></td>
-                    <td style="border: 0px white solid;vertical-align: top;text-align: justify;">Dalam mencapai tujuan yang ditetapkan, Koperasi secara langsung maupun melalui unit-unitnya dapat melakukan <b style="font-size: 19px;">kegiatan sebagai berikut:</b>
-                <ul>
-                  <li style="color: #f7a91e;text-align: justify;"><font color="black">Menyediakan jasa pendidikan dan pelatihan baik Pendidikan formal dan non formal.</font></li>
-                  <li style="color: #f7a91e;text-align: justify;"><font color="black">Memberikan jasa konsultasi bisnis baik untuk anggotanya dan pihak ketiga.</font></li>
-                  <li style="color: #f7a91e;text-align: justify;"><font color="black">Memberikan jasa konsultasi bisnis bagi entitas bisnis para anggota atau pihak ketiga</font></li>
-                  <li style="color: #f7a91e;text-align: justify;"><font color="black">Melakukan penyertaan usah/modal pada bisnis yang dilakukan oleh para anggotanya atau pihak ketiga.</font></li>
-                  <li style="color: #f7a91e;text-align: justify;"><font color="black">Melakukan kerjasama bisnis atau patungan dengan pihak ketiga dan/atau</font></li>
-                  <li style="color: #f7a91e;text-align: justify;"><font color="black">Kegiatan jasa lainnya yang diperbolehkan sesuai dengan ketentuan peraturan perundang-undangan yang berlaku</font></li>
-                </ul> </td>
-                  </tr>
-                </table>
-
-
-
-                  
-                </div>
-              </div>
+              <h4 class="widget-title clearfix"><span>
+                  <font color="#f7a91e">Koperasi Jasa Komunitas Panen Saham Indonesia</font>
+                </span></h4>
+              <p align="center">merupakan salah satu upaya sebagai koperasi konsumen, namun juga mampu meningkatkan
+                kinerja melalui anak perusahaan yang bergerak dalam berbagai bidang.</p>
 
             </div>
-            <div class="clearfix"></div>
+            <!-- //  Mail list -->
+
           </div>
-          <!-- // post -->
+          <!-- // sidebar -->
+
+          <!--  content -->
+          <div class="col-lg-8 col-md-8 sticky-content">
+
+            <!-- post -->
+            <div class="blog-entry background-white border-1 border-grey-1 margin-bottom-35px">
+              <div class="row">
+                <div class="col-lg-3"><img src="<?= base_url('assets/img/about/') ?>visi.png"
+                    style="width: 70%;padding-left: 30px;"></div>
+                <div class="col-lg-9">
+                  <div class="padding-25px">
+                    <h3>Menjadi koperasi jasa yang <b style="color: #603913;">berkualitas</b> dan <b style="color: #603913;">terbaik</b> di Indonesia.</h3>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-lg-3"><img src="<?= base_url('assets/img/about/') ?>misi.png"
+                    style="width: 70%;padding-left: 30px;"></div>
+                <div class="col-lg-9" style="border: 0px white solid;">
+
+                  <table style="border: 0px white solid;width: 100%;">
+                    <tr>
+                      <td style="border: 0px white solid;width: 10%;vertical-align: top;"><img
+                          src="<?= base_url('assets/img/about/') ?>ceklis.png"></td>
+                      <td style="border: 0px white solid;width: 90%;vertical-align: top;text-align: justify;"><b
+                          style="font-size: 19px; color: #603913;">Mendidik</b> para anggotanya agar dapat menemukan potensinya dan
+                        memberdayakan diri melalui koperasi.</td>
+                    </tr>
+                    <tr>
+                      <td style="border: 0px white solid;vertical-align: top;"><img
+                          src="<?= base_url('assets/img/about/') ?>ceklis.png"></td>
+                      <td style="border: 0px white solid;vertical-align: top;text-align: justify;"><b
+                          style="font-size: 19px; color: #603913;">Menciptakan</b> kesejahteraan bagi para anggota yang
+                        berkesinambungan. </td>
+                    </tr>
+                    <tr>
+                      <td style="border: 0px white solid;vertical-align: top;"><img
+                          src="<?= base_url('assets/img/about/') ?>ceklis.png"></td>
+                      <td style="border: 0px white solid;vertical-align: top;text-align: justify;"><b
+                          style="font-size: 19px; color: #603913;">Memberikan</b> kontribusi bagi perkembangan koperasi di Indonesia.
+                      </td>
+                    </tr>
+                    <tr>
+                      <td style="border: 0px white solid;vertical-align: top;"><img
+                          src="<?= base_url('assets/img/about/') ?>ceklis.png"></td>
+                      <td style="border: 0px white solid;vertical-align: top;text-align: justify;"><b
+                          style="font-size: 19px; color: #603913;">Mengelola</b> unit-unit usahanya secara professional dan akuntabel
+                        dengan menerapkan prinsip-prinsip pengelolaan perusahaan yang baik. </td>
+                    </tr>
+                    <tr>
+                      <td style="border: 0px white solid;vertical-align: top;"><img
+                          src="<?= base_url('assets/img/about/') ?>ceklis.png"></td>
+                      <td style="border: 0px white solid;vertical-align: top;text-align: justify;"><b
+                          style="font-size: 19px; color: #603913;">Menjadi Mitra</b> terpercaya dan strategis bagi setiap anggota KJKPI
+                      </td>
+                    </tr>
+                  </table>
+                </div>
+              </div>
 
 
-              
+              <div class="row">
+
+                <div class="col-lg-3"><img src="<?= base_url('assets/img/about/') ?>tujuan.png"
+                    style="padding-left: 30px;"></div>
+              </div>
+              <br>
+              <div class="col-lg-12" style="border: 0px white solid;padding-left: 30px;">
+
+                <table style="border: 0px white solid;width: 100%;">
+                  <tr>
+                    <td style="border: 0px white solid;width: 8%;vertical-align: top;"><img
+                        src="<?= base_url('assets/img/about/') ?>ceklis.png"></td>
+                    <td style="border: 0px white solid;width: 90%;vertical-align: top;text-align: justify;"><b
+                        style="font-size: 19px; color: #603913;">Koperasi bertujuan</b> meningkatkan kesejahteraan anggota pada
+                      khususnya dan masyarakat pada umumnya, sekaligus pada bagian yang tidak terpisahkan dari <b
+                        style="font-size: 19px; color: #603913;">tatanan perekonomian nasional demokratis dan berkeadilan.</b></td>
+                  </tr>
+                  <tr>
+                    <td style="border: 0px white solid;vertical-align: top;"><img
+                        src="<?= base_url('assets/img/about/') ?>ceklis.png"></td>
+                    <td style="border: 0px white solid;vertical-align: top;text-align: justify;">Dalam mencapai tujuan
+                      yang ditetapkan, Koperasi secara langsung maupun melalui unit-unitnya dapat melakukan <b
+                        style="font-size: 19px; color: #603913;">kegiatan sebagai berikut:</b>
+                      <ul>
+                        <li style="color: #f7a91e;text-align: justify;">
+                          <font color="black">Menyediakan jasa pendidikan dan pelatihan baik Pendidikan formal dan non
+                            formal.</font>
+                        </li>
+                        <li style="color: #f7a91e;text-align: justify;">
+                          <font color="black">Memberikan jasa konsultasi bisnis baik untuk anggotanya dan pihak ketiga.
+                          </font>
+                        </li>
+                        <li style="color: #f7a91e;text-align: justify;">
+                          <font color="black">Memberikan jasa konsultasi bisnis bagi entitas bisnis para anggota atau
+                            pihak ketiga</font>
+                        </li>
+                        <li style="color: #f7a91e;text-align: justify;">
+                          <font color="black">Melakukan penyertaan usah/modal pada bisnis yang dilakukan oleh para
+                            anggotanya atau pihak ketiga.</font>
+                        </li>
+                        <li style="color: #f7a91e;text-align: justify;">
+                          <font color="black">Melakukan kerjasama bisnis atau patungan dengan pihak ketiga dan/atau
+                          </font>
+                        </li>
+                        <li style="color: #f7a91e;text-align: justify;">
+                          <font color="black">Kegiatan jasa lainnya yang diperbolehkan sesuai dengan ketentuan peraturan
+                            perundang-undangan yang berlaku</font>
+                        </li>
+                      </ul>
+                    </td>
+                  </tr>
+                </table>
+
+
+
+
+              </div>
+            </div>
+
+          </div>
+          <div class="clearfix"></div>
+        </div>
+        <!-- // post -->
+
+
+
         <!-- //  content -->
 
       </div>
     </div>
-  </div>
-  <!-- //  page output -->
+    </div>
+    <!-- //  page output -->
 
-  <style>
-    .no-border {
-      border-color: transparent;
-    }
+    <style>
+      .no-border {
+        border-color: transparent;
+      }
 
-    .no-bg {
-      background-color: transparent;
-    }
-    .section-bg2{
-      background-color: #efebe8;
-    }
-    
+      .no-bg {
+        background-color: transparent;
+      }
+
+      .section-bg2 {
+        background-color: #efebe8;
+      }
+
       .kontak-heading {
         border-bottom: 4px solid #ddaa37;
         width: max-content;
       }
-      .text-primary{
+
+      .text-primary {
         color: #795839 !important;
       }
     </style>
